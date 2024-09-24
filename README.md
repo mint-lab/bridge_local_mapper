9718843f547dd39d898ad5859019400b95a9e689
Sunglok Choi <sunglok@hanmail.net>
2024-09-24 09:22:45 +0900

    FEAT: Publish `PointStamped` for a front obstacle

--- README.md
+++ README.md
@@ -6,17 +6,15 @@ _bridge\_local\_planner_는 NRF-Bridge 프로젝트에서 **지역 지도 작성
 
 ### 1. 주요 기술 소개
 * **Local mapping**
-  * `gtrack_mapper.py`: Local mapper with ground plane constraints, asymmetric MSAC, and plane tracking (**추천**)
-    * 지면의 제약조건과 비대칭 MSAC, 그리고 평면 추적을 모두 이용한 지면 검출과 장애물 분리를 이용한 local mapping 알고리즘
   * `o3d_mapper.py`: Local mapper using Open3D plane detection
     * 지면 검출에 Open3D의 RANSAC 기반 plane detection을 이용한 local mapping 알고리즘
-    * 매개변수
+    * Parameters
       * `ransac_num_iters`: RANSAC 기반 plane detection의 (최대) 반복 횟수 (기본값: 1000)
       * `ransac_num_samples`: Plane fitting에 사용될 점의 개수 (기본값: 3)
       * `ransac_threshold`: Plane fitting 결과와 점 사이의 거리 임계값 (기본값: 0.05, 단위: [m])
   * `gconst_mapper.py`: Local mapper with ground plane constraints
     * 지면의 제약조건과 RANSAC의 adaptive termination을 적용한 local mapping 알고리즘
-    * 추가 매개변수
+    * Additional parameters
       * `ransac_min_iters`: RANSAC의 최소 반복 횟수 (기본값: 10)
       * `ransac_confidence`: RANSAC의 반복 횟수를 계산하기 위한 신뢰도 값 (기본값: 0.99)
       * `ransac_refinement`: RANSAC 후에 inlier 점들을 이용한 plane fitting 재수행 여부 (기본값: True)
@@ -25,6 +23,8 @@ _bridge\_local\_planner_는 NRF-Bridge 프로젝트에서 **지역 지도 작성
       * `plane_max_height`: Plane fitting 결과와 로봇좌표계 사이의 최대 직선거리 (기본값: 1.5, 단위: [m])
   * `gmsac_mapper.py`: Local mapper with ground plane constraints and MSAC
     * 지면의 제약조건과 MSAC을 적용한 local mapping 알고리즘
+  * `gtrack_mapper.py`: Local mapper with ground plane constraints, asymmetric MSAC, and plane tracking <span style="color:red">(recommended)</span>
+    * 지면의 제약조건과 비대칭 MSAC, 그리고 평면 추적을 모두 이용한 지면 검출과 장애물 분리를 이용한 local mapping 알고리즘
   
 * **Local path planning**
   * `straight_planner.py`
@@ -47,14 +47,14 @@ _bridge\_local\_planner_는 NRF-Bridge 프로젝트에서 **지역 지도 작성
   mapper = GTrackMapper()
   mapper.set_params({'pts_sampling_step': 2, 'debug_info': True})
   
-  # Prepare a point cloud
+  # Generate an example point cloud
   pts = generate_pointcloud()
   
   # Apply the point cloud
   success = mapper.apply_pointcloud(pts)
   
   # Access the updated map data
-  r, c = mapper.conv_xy2rc(3, 2) # meter to index
+  r, c = mapper.conv_xy2rc(3, 2) # Convert (3, 2) meters to its corresponding index
   is_object = mapper.map_data['obstacles'][r, c] == 0
   elevation = mapper.map_data['elevation'][r, c]
   histogram = mapper.map_data['histogram'][r, c]
@@ -72,23 +72,48 @@ _bridge\_local\_planner_는 NRF-Bridge 프로젝트에서 **지역 지도 작성
   * `local_mapper_node.py`: Local mapper wrapper (ROS 2 node)
     * Local map을 `nav_msgs/OccupancyGrid` 메세지로 publish하는 ROS 2 node
     * Subscribed topics
-      * Point cloud: `local_mapper_node/point_cloud` (type: `sensor_msgs/PointCloud2`, unit: [m]) 
+      * Point cloud: `local_mapper_node/point_cloud`
+        * Message type: `sensor_msgs/PointCloud2`
+        * Unit: [m]
     * Publishing topics
-      * Obstacles map: `local_mapper_node/obstacles_map` (type: `nav_msgs/OccupancyGrid`)
+      * Obstacles map: `local_mapper_node/obstacles_map`
+        * Message type: `nav_msgs/OccupancyGrid`
         * Values: -1 (unknown), 0 (empty), 1-100 (occupancy probability)
-      * Elevation map: `local_mapper_node/elevation_map` (type: `nav_msgs/OccupancyGrid`)
+        * Unit: [%]
+      * Elevation map: `local_mapper_node/elevation_map`
+        * Message type: `nav_msgs/OccupancyGrid`
         * Value range: [-128, 127]
         * Unit: [cm]
-      * Histogram map: `local_mapper_node/histogram_map` (type: `nav_msgs/OccupancyGrid`)
+      * Histogram map: `local_mapper_node/histogram_map`
+        * Message type: `nav_msgs/OccupancyGrid`
         * Value range: [0, 127]
+      * Front (first) obstacle: `local_mapper_node/front_obstacle`
+        * Message type: `geometry_msgs/Point`
+        * Value examples: No obstacle (-1, 0, -1), Obstacle (distance, 0, height)
+        * Unit: [m]
+    * Parameters
+      * `map_x_length`: 지도의 X 방향 길이 (Default: `10`, Unit: [m])
+      * `map_y_length`: 지도의 Y 방향 길이 (Default: `10`, Unit: [m])
+      * `map_cellsize`: 지도의 셀의 크기 (Default: `0.1`, Unit: [m])
+      * `map_frame_id`: Publish되는 지도의 frame ID (Default: `"map"`)
+      * `elevation_scale`: Elevation map의 스케일 (Default: `100`)
+        * 참고) 스케일 값이 100으로 설정된 경우, [m] 단위의 값을 100배하여 [cm] 단위로 바꾼다.
+      * `verbose`: 알고리즘의 실행시간 등 추가적인 정보를 화면에 출력 (Default: `True`)
+      * `config_params`: 위의 1장에 설명된 세부 parameters를 YAML 형식의 텍스트로 설정 가능 (Default: `""`)
+        * 설정 예: [config/bridge_orin.yaml](https://github.com/mint-lab/bridge_local_planner/blob/master/config/bridge_orin.yaml) 파일 내 `config_params` 항목
+      * `config_file`: 위의 1장에 설명된 세부 parameters를 YAML 파일로 설정 가능 (Default: `""`)
 
 
 
 #### 3.3. ROS 2 환경
 * **Local mapper 노드 실행**
-  * `ros2 run bridge_planner local_mapper_node [--ros-args --remap /local_mapper_node/point_cloud:=/your/published/point_cloud/topic]`
-    * `--remap` 옵션:  노드에 입력할 point cloud 토픽 설정
-      * 예: `--remap /local_mapper_node/point_cloud:=/zed2i/zed_node/point_cloud/cloud_registered`
+  * `ros2 run bridge_local_planner local_mapper_node [--ros-args <options>]`
+    * `--remap`  (`-r`) 옵션:  노드에 입력할 토픽 이름 맵핑
+      * 사용 예: `--remap /local_mapper_node/point_cloud:=/zed/zed_node/point_cloud/cloud_registered`
+    * `--params_file` 옵션: 노드에 설정할 파라미터들이 정의된 파일 설정
+      * 사용 예: `--params-file src/bridge_local_planner/config/bridge_orin.yaml`
+    * `--param` (`-p`) 옵션: 위의 3.2 절에 소개된 세부 parameters 설정 가능
+      * 사용 예: `--param map_frame_id:="robot"`
   * rviz2에서 지도 가시화 가능
 
 
--- bridge_local_planner/local_mapper_node.py
+++ bridge_local_planner/local_mapper_node.py
@@ -1,5 +1,6 @@
 # Python packages
 import numpy as np
+import yaml
 
 # ROS 2 packages
 import rclpy
@@ -7,6 +8,7 @@ from rclpy.node import Node
 import sensor_msgs_py.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2
 from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PointStamped
 
 # Local packages
 try:
@@ -28,25 +30,33 @@ class LocalMapperNode(Node):
         self.declare_parameter('map_x_length', 10)
         self.declare_parameter('map_y_length', 10)
         self.declare_parameter('map_cellsize', 0.1)
-        self.declare_parameter('elevation_alpha', 100)
+        self.declare_parameter('map_frame_id', 'map')
+        self.declare_parameter('elevation_scale', 100)
+        self.declare_parameter('verbose', True)
+        self.declare_parameter('config_params', '')
         self.declare_parameter('config_file', '')
 
         self.map_x_length = float(self.get_parameter('map_x_length').value)
         self.map_y_length = float(self.get_parameter('map_y_length').value)
         self.map_cellsize = float(self.get_parameter('map_cellsize').value)
-        self.elevation_alpha = float(self.get_parameter('elevation_alpha').value)
+        self.map_frame_id = str(self.get_parameter('map_frame_id').value)
+        self.elevation_scale = float(self.get_parameter('elevation_scale').value)
+        self.verbose = bool(self.get_parameter('verbose').value)
 
         # Initialize the local mapper.
         self.mapper = GTrackMapper(self.map_x_length, self.map_y_length, self.map_cellsize)
         self.mapper.set_params({
             'robot2sensor_T'    : np.eye(4),
             'pts_sampling_step' : 4,
-            'ground_correct'    : True,
-            'ground_mapping'    : True,
-        })
+        }) # The default paraeters for ROS 2 environment
+
+        config_params = self.get_parameter('config_params').value
+        if config_params:
+            config_dict = yaml.safe_load(config_params)
+            self.mapper.set_params(config_dict)
         config_file = self.get_parameter('config_file').value
         if config_file:
-            self.mapper.load_config(config_file)
+            self.mapper.load_params_from_yaml(config_file)
 
         # Initialize the local (occupancy grid) maps.
         self.map_msg = {}
@@ -62,12 +72,15 @@ class LocalMapperNode(Node):
             self.map_array[layer] = None
 
         # Initialize subscribers and publishers.
-        self.pcd_subscriber = self.create_subscription(PointCloud2, '/local_mapper_node/point_cloud', self.subscribe_pointcloud2, 2)
+        self.node_name = self.get_name()
+        self.pcd_subscriber = self.create_subscription(PointCloud2, f'/{self.node_name}/point_cloud', self.subscribe_pointcloud2, 2)
         self.map_publishers = {}
         for layer in self.mapper.map_data:
-            self.map_publishers[layer] = self.create_publisher(OccupancyGrid, f'/local_mapper_node/{layer}_map', 2)
+            self.map_publishers[layer] = self.create_publisher(OccupancyGrid, f'/{self.node_name}/{layer}_map', 2)
+        self.obs_publisher = self.create_publisher(PointStamped, f'/{self.node_name}/front_obstacle', 2)
 
-        self.get_logger().info('[local_mapper_node] Started.')
+        if self.verbose:
+            self.get_logger().info(f'[{self.node_name}] Started.')
 
     @staticmethod
     def conv_pointcloud2_to_array(pcd_msg):
@@ -78,33 +91,55 @@ class LocalMapperNode(Node):
         return pts
 
     def subscribe_pointcloud2(self, msg):
-        """Subscribe a PointCloud2 messages (callback function)."""
+        """Subscribe a PointCloud2 message (callback function)."""
         pts = self.conv_pointcloud2_to_array(msg)
+        if len(pts) <= 0:
+            self.get_logger().warn(f'[{self.node_name}] Empty point cloud.')
 
         # Apply the point cloud to the local mapper.
         start_time = self.get_clock().now()
         success = self.mapper.apply_pointcloud(pts)
         elapsed_time = self.get_clock().now() - start_time
-        self.get_logger().info(f'[local_mapper_node] Computing time: {elapsed_time.nanoseconds / 1e6} [msec]')
+        if self.verbose:
+            self.get_logger().info(f'[{self.node_name}] Computing time: {elapsed_time.nanoseconds / 1e6} [msec]')
         if not success:
-            self.get_logger().warn('[local_mapper_node] Failed to apply the point cloud.')
+            self.get_logger().warn(f'[{self.node_name}] Failed to apply the point cloud.')
 
-        # Publish the local maps.
+        # Publish the local maps and the front obstacle.
         self.publish_occupancy_maps(msg.header.stamp)
+        self.publish_front_obstacle(msg.header.stamp)
 
     def publish_occupancy_maps(self, timestamp):
+        """Publish the local maps as OccupancyGrid messages."""
         # Update the map messages.
         self.map_array['obstacles'] = self.mapper.map_data['obstacles']
-        self.map_array['elevation'] = (self.mapper.map_data['elevation'] * self.elevation_alpha).clip(-128, 127).astype(np.int8)
+        self.map_array['elevation'] = (self.mapper.map_data['elevation'] * self.elevation_scale).clip(-128, 127).astype(np.int8)
         self.map_array['histogram'] = self.mapper.map_data['histogram'].clip(0, 127).astype(np.int8)
 
         # Publish the map messages.
         for layer in self.map_msg:
             map = self.map_msg[layer]
-            map.header.frame_id = 'map' # TODO: 'local_map'
+            map.header.frame_id = self.map_frame_id
             map.header.stamp = timestamp
             map.data = self.map_array[layer].flatten().tolist()
             self.map_publishers[layer].publish(map)
+
+    def publish_front_obstacle(self, timestamp):
+        """Publish the front obstacle as a Point message."""
+        r_center, c_center = self.mapper.conv_xy2rc(0, 0)
+        c_length = len(self.mapper.map_data['obstacles'][0])
+        obs = PointStamped()
+        obs.header.stamp = timestamp
+        obs.header.frame_id = self.map_frame_id
+        obs.point.x = -1.
+        obs.point.y = 0.
+        obs.point.z = -1.
+        for c in range(c_center, c_length):
+            if self.mapper.map_data['obstacles'][r_center, c] > 50:
+                obs.point.x, _ = self.mapper.conv_rc2xy(r_center, c)
+                obs.point.z = float(self.mapper.map_data['elevation'][r_center, c])
+                break
+        self.obs_publisher.publish(obs)
 
 
 def main(args=None):
